4dd97d5c1a5e09370a129ef094a95b77d6d74d01
서유진 <97.eugene.s@gmail.com>
2023-10-30 18:08:04 +0900

    23년 10월 30일
    수업 업데이트

---
+++ README.md
@@ -0,0 +1,6 @@
+# Vew로 사이트 만들기
+
+## 미리보기
+
+## 설치
+`npm create vue@latest`
---
+++ src/views/HomeView.vue
@@ -0,0 +1,23 @@
+<script setup>
+import Header from "@/compononts/Header.vue"
+import Footer from "@/compononts/Footer.vue"
+import Slider from "@/compononts/Slider.vue"
+import Image from "@/compononts/Image.vue"
+import ImageText from "@/compononts/ImageText.vue"
+import Card from "@/compononts/Card.vue"
+import Banner from "@/compononts/Banner.vue"
+import Text from "@/compononts/Text.vue"
+</script>
+
+<template>
+  <Header />
+  <main>
+    <Slider />
+    <Image />
+    <ImageText />
+    <Card />
+    <Banner />
+    <Text />
+  </main>
+  <Footer />
+</template>
